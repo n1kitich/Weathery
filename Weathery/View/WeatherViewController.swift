947302dcce666648cc084fd4571909e34aa0183f
n1kitich <76942708+n1kitich@users.noreply.github.com>
2021-06-27 16:57:29 +0300

    Create Xcode project

---
+++ Weathery/View/WeatherViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherViewController.swift
+//  Weathery
+//
+//  Created by Anon Account on 27.06.2021.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
