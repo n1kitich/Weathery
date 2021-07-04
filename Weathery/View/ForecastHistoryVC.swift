c5f30a20e947a821fa7a910638003f66cf571eee
n1kitich <76942708+n1kitich@users.noreply.github.com>
2021-07-04 20:53:25 +0300

    Add data display to forecast history

--- Weathery/View/ForecastHistoryVC.swift
+++ Weathery/View/ForecastHistoryVC.swift
@@ -6,16 +6,22 @@
 //
 
 import UIKit
+import CoreData
 
-class ForecastHistoryVC: UIViewController {
+class ForecastHistoryVC: UIViewController, NSFetchedResultsControllerDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    var managedObjectContext: NSManagedObjectContext!
+    var fetchedResultsController = NSFetchedResultsController<NSFetchRequestResult>()
     
     let cellID = "cellIdentifier"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).managedObjectContext
+        
         setupTableView()
+        setupFetchedController()
     }
     
     func setupTableView() {
@@ -23,17 +29,27 @@ class ForecastHistoryVC: UIViewController {
         tableView.dataSource = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
     }
+    
+    func setupFetchedController() {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Weather")
+        let sortDescriptor = NSSortDescriptor(key: "descript", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+    }
 
 }
 
 extension ForecastHistoryVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        let numberOfRows = fetchedResultsController.sections?[section].numberOfObjects ?? 0
+        return numberOfRows
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        let object = fetchedResultsController.object(at: indexPath) as! Weather
+        cell.textLabel?.text = object.descript
         return cell
     }
     
