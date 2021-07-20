0bb2062e6b3ce244ae606047169d11ffd389c423
n1kitich <76942708+n1kitich@users.noreply.github.com>
2021-07-20 21:01:22 +0300

    Add a check for the existence of a data model

--- Weathery/View/WeatherViewController.swift
+++ Weathery/View/WeatherViewController.swift
@@ -74,27 +74,30 @@ class WeatherViewController: UIViewController {
     }
     
     func displayData() {
-        placeLabel.text = weatherModel?.location.name
-        updatedTimeLabel.text = weatherModel?.location.localtime
-        descriptionLabel.text = weatherModel?.current.weatherDescriptions.first
-        temperatureLabel.text = String((weatherModel?.current.temperature)!)
-        feellikeLabel.text = "Feel like: " + String((weatherModel?.current.feelslike)!)
-        windLabel.text = String((weatherModel?.current.windSpeed)!)
-        pressureLabel.text = String((weatherModel?.current.pressure)!)
-        humidityLabel.text = String((weatherModel?.current.humidity)!)
+        guard let weatherModel = weatherModel else { return }
+        
+        placeLabel.text = weatherModel.location.name
+        updatedTimeLabel.text = weatherModel.location.localtime
+        descriptionLabel.text = weatherModel.current.weatherDescriptions.first
+        temperatureLabel.text = String(weatherModel.current.temperature)
+        feellikeLabel.text = "Feel like: " + String(weatherModel.current.feelslike)
+        windLabel.text = String(weatherModel.current.windSpeed)
+        pressureLabel.text = String(weatherModel.current.pressure)
+        humidityLabel.text = String(weatherModel.current.humidity)
     }
     
     func saveDataToStore() {
+        guard let weatherModel = weatherModel else { return }
+        
         let weather = NSEntityDescription.insertNewObject(forEntityName: "Weather", into: coreDataManager.managedContext) as! Weather
         let request = NSEntityDescription.insertNewObject(forEntityName: "Request", into: coreDataManager.managedContext) as! Request
         let location = NSEntityDescription.insertNewObject(forEntityName: "Location", into: coreDataManager.managedContext) as! Location
         let current = NSEntityDescription.insertNewObject(forEntityName: "Current", into: coreDataManager.managedContext) as! Current
                 
-        location.name = weatherModel?.location.name
-        location.localtime = weatherModel?.location.localtime
-        //shoud fix
-        current.temperature = (weatherModel?.current.temperature)!
-        current.weatherDescriptions = weatherModel?.current.weatherDescriptions.first
+        location.name = weatherModel.location.name
+        location.localtime = weatherModel.location.localtime
+        current.temperature = Int16(weatherModel.current.temperature)
+        current.weatherDescriptions = weatherModel.current.weatherDescriptions.first
                 
         weather.location = location
         weather.request = request
@@ -106,7 +109,6 @@ class WeatherViewController: UIViewController {
     func searchWeather(by place: String) {
         dataFetcher.fetchData(urlString: "http://api.weatherstack.com/current", accessKey: "503bc119ccde64a16ae23720599aa21f", query: place) { weatherForecast in
             self.weatherModel = weatherForecast
-            self.saveDataToStore()
             self.displayData()
         }
     }
@@ -121,6 +123,7 @@ extension WeatherViewController: UISearchBarDelegate {
         searchBar.resignFirstResponder()
         if let text = searchBar.text {
             searchWeather(by: text)
+            saveDataToStore()
         }
     }
     
@@ -147,6 +150,6 @@ extension WeatherViewController: CLLocationManagerDelegate {
         }
     }
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        updatedTimeLabel.text = "Can`t find your position"
+        updatedTimeLabel.text = "Can`t find your current position"
     }
 }
