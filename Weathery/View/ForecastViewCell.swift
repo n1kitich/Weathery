86a3bd1bb06ddf049da4f0c9cd414ca7757cabc9
n1kitich <76942708+n1kitich@users.noreply.github.com>
2021-07-20 20:55:18 +0300

    Delete awakeFromNib function

--- Weathery/View/ForecastViewCell.swift
+++ Weathery/View/ForecastViewCell.swift
@@ -14,26 +14,15 @@ class ForecastViewCell: UITableViewCell {
     @IBOutlet weak var tempLabel: UILabel!
     @IBOutlet weak var descriptLabel: UILabel!
     
-    
     static let cellIdentifier = "Cell"
     
     static func nib() -> UINib {
         return UINib(nibName: "ForecastViewCell", bundle: nil)
     }
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
     }
-    
-//    func configure(temperature: String, descript: String) {
-//        tempLabel.text = temperature
-//        descriptLabel.text = descript
-//    }
     
 }
