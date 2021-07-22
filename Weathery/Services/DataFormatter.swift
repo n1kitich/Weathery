32d3c4ef54444bd89483cafe230a934efc64c4ea
n1kitich <76942708+n1kitich@users.noreply.github.com>
2021-07-22 22:07:04 +0300

    Add a data formatter

---
+++ Weathery/Services/DataFormatter.swift
@@ -0,0 +1,21 @@
+//
+//  DataFormatter.swift
+//  Weathery
+//
+//  Created by Anon Account on 22.07.2021.
+//
+
+import Foundation
+
+extension Int {
+    func getDateStringFromUnix() -> String {
+        let date = Date(timeIntervalSince1970: TimeInterval(self))
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US")
+        dateFormatter.dateFormat = "MMM d, h:mm a"
+
+        return dateFormatter.string(from: date)
+    }
+}
+
