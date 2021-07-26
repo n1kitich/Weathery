bd3d09dd21525411652ebc12d94313ea17af5469
n1kitich <76942708+n1kitich@users.noreply.github.com>
2021-07-26 21:58:37 +0300

    Add outlets for new UI objects and alpha-value for it

--- Weathery/SearchHistory/SearchHistoryViewCell.swift
+++ Weathery/SearchHistory/SearchHistoryViewCell.swift
@@ -13,10 +13,22 @@ class SearchHistoryViewCell: UITableViewCell {
     @IBOutlet weak var placeLabel: UILabel!
     @IBOutlet weak var tempLabel: UILabel!
     @IBOutlet weak var descriptLabel: UILabel!
+    @IBOutlet weak var temperature: UILabel!
+    @IBOutlet weak var descript: UILabel!
     
     static let cellIdentifier = "Cell"
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupLabel()
+    }
+    
     static func nib() -> UINib {
         return UINib(nibName: "SearchHistoryViewCell", bundle: nil)
+    }
+    
+    private func setupLabel() {
+        temperature.textColor = temperature.textColor?.withAlphaComponent(0.6)
+        descript.textColor = descript.textColor?.withAlphaComponent(0.6)
     }
 }
