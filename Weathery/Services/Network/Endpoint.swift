b07ddbf098b3bbb8618da2e9ae7ce2b73745469d
n1kitich <76942708+n1kitich@users.noreply.github.com>
2021-07-29 00:44:20 +0300

    Add processing for query type

--- Weathery/Services/Network/Endpoint.swift
+++ Weathery/Services/Network/Endpoint.swift
@@ -7,21 +7,33 @@
 
 import Foundation
 
-
-struct Endpoint {
-    let query: String
+struct Endpoint<T> {
     let accessKey: String
-    
+    let query: T
+
     var url: URL? {
         var urlComponents = URLComponents()
         urlComponents.scheme = "https"
         urlComponents.host = "api.openweathermap.org"
         urlComponents.path = "/data/2.5/weather"
-        urlComponents.queryItems = [
-            URLQueryItem(name: "q", value: query),
-            URLQueryItem(name: "units", value: "metric"),
-            URLQueryItem(name: "appid", value: accessKey)
-        ]
+        
+        if let simpleQuery = query as? String {
+            urlComponents.queryItems = [
+                URLQueryItem(name: "q", value: simpleQuery),
+                URLQueryItem(name: "units", value: "metric"),
+                URLQueryItem(name: "appid", value: accessKey)
+            ]
+            return urlComponents.url
+        }
+        if let coordinates = query as? (String, String) {
+            urlComponents.queryItems = [
+                URLQueryItem(name: "lat", value: coordinates.0),
+                URLQueryItem(name: "lon", value: coordinates.1),
+                URLQueryItem(name: "units", value: "metric"),
+                URLQueryItem(name: "appid", value: accessKey)
+            ]
+        }
         return urlComponents.url
     }
 }
+
